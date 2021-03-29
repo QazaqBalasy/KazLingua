9b3285c940c94ef60108f50964161cd8a45a5c76
Sunny <sundet.kz@gmail.com>
2021-03-29 23:15:25 +0600

    Initial Commit

---
+++ KazLinguo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KazLinguo
+//
+//  Created by Sundet Mukhtar on 3/29/21.
+//  Copyright © 2021 SKE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
